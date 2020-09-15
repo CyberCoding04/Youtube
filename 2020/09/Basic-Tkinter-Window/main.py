9afc22ec427dac4590811903724b0c84c75f550c
Divyansh Agrawal <68465845+CyberCoding04@users.noreply.github.com>
2020-09-15 08:53:59 +0530

    Create main.py

---
+++ 2020/09/Basic-Tkinter-Window/main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+#Python3 - from 'tkinter'
+#Python2 - from 'Tkinter'
+
+window = Tk() # initialising a window
+window.title("Second Window") # Setting the title of the window
+
+window.geometry('{}x{}'.format(500,500))
+
+window.mainloop()
